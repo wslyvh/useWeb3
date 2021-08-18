aa84362345d3008f1e30134af22bd0f25fb2bc55
wslyvh <wslyvh@users.noreply.github.com>
2021-08-18 09:36:16 +0200

    SEO OG

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
-import { DESCRIPTION, IMAGE_OG, TITLE } from 'utils/constants'
+import { DESCRIPTION, IMAGE_OG, SITE_URL, TITLE } from 'utils/constants'
 
 interface SEOProps {
   title?: string
@@ -10,8 +10,8 @@ interface SEOProps {
 
 export function SEO(props: SEOProps) {
   const title = props.title ? `${props.title} · ${TITLE}` : `${TITLE} · Learn Web3 development`
-  const description = props.description || DESCRIPTION
-  const image = props.imageUrl || IMAGE_OG
+  const description = props.description ?? DESCRIPTION
+  const image = props.imageUrl ?? IMAGE_OG
 
   return (
     <Head>
@@ -19,9 +19,12 @@ export function SEO(props: SEOProps) {
       <meta name="description" content={description} key="description" />
       <meta name="image" content={image} key="image" />
 
+      <meta property="og:type" content="website" />
       <meta property="og:title" content={title} key="og_title" />
       <meta property="og:description" content={description} key="og_description" />
+      <meta property="og:url" content={SITE_URL} key="og_url" />
       <meta property="og:image" content={image} key="og_image" />
+      <meta property="og:image:secure_url" content={image.replace('https://', 'http://')} key="og_image_secure_url" />
 
       <meta name="twitter:card" content="summary_large_image" key="tw_card" />
       <meta name="twitter:title" content={title} key="tw_title" />
