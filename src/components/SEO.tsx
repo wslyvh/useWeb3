2371922eced6285c83ed11f99a95b975ae9d417d
wslyvh <wslyvh@users.noreply.github.com>
2021-08-18 10:31:30 +0200

    image property

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -13,7 +13,8 @@ export function SEO(props: SEOProps) {
   const router = useRouter()
   const title = props.title ? `${props.title} · ${TITLE}` : `${TITLE} · Learn Web3 development`
   const description = props.description ?? DESCRIPTION
-  const image = props.imageUrl ?? IMAGE_OG
+  const image_secure = props.imageUrl ?? IMAGE_OG
+  const image = image_secure.replace('https://', 'http://')
   const url = SITE_URL.replace(/\/$/, '') + router.asPath.split('?')[0]
 
   return (
@@ -22,13 +23,13 @@ export function SEO(props: SEOProps) {
       <meta name="description" content={description} key="description" />
       <meta name="image" content={image} key="image" />
 
-      <meta name="og:type" content="website" key="og_type" />
-      <meta name="og:site_name" content={TITLE} key="og_site_name" />
-      <meta name="og:title" content={title} key="og_title" />
-      <meta name="og:description" content={description} key="og_description" />
-      <meta name="og:url" content={url} key="og_url" />
-      <meta name="og:image" content={image.replace('https://', 'http://')} key="og_image" />
-      <meta name="og:image:secure_url" content={image} key="og_image_secure_url" />
+      <meta property="og:type" content="website" key="og_type" />
+      <meta property="og:site_name" content={TITLE} key="og_site_name" />
+      <meta property="og:title" content={title} key="og_title" />
+      <meta property="og:description" content={description} key="og_description" />
+      <meta property="og:url" content={url} key="og_url" />
+      <meta property="og:image" content={image} key="og_image" />
+      <meta property="og:image:secure_url" content={image_secure} key="og_image_secure_url" />
 
       <meta name="twitter:card" content="summary_large_image" key="tw_card" />
       <meta name="twitter:title" content={title} key="tw_title" />
