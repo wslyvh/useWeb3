73773a4447aa6c560917d941cec621888bdf8cfa
wslyvh <wslyvh@users.noreply.github.com>
2021-08-25 11:03:46 +0200

    plausible

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import 'assets/styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { SEO } from 'components/SEO'
+import PlausibleProvider from 'next-plausible'
+import { DOMAIN } from 'utils/constants'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <PlausibleProvider domain={DOMAIN}>
       <SEO />
       <Component {...pageProps} />
-    </>
+    </PlausibleProvider>
   )
 }
+
 export default MyApp
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,6 +3,7 @@ export const DESCRIPTION = 'A curated overview of the best and latest resources 
 export const SITE_URL = 'https://useweb3.xyz/'
 export const IMAGE_OG = `${SITE_URL}images/social.png`
 export const SOCIAL_HANDLE = '@useWeb3'
+export const DOMAIN = 'useweb3.xyz'
 
 export const GA_TRACKING_ID = 'G-FNPWLTEDV7'
 export const DEFAULT_REVALIDATE_PERIOD = 600
