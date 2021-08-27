6dd6427a0e5b1704bf660764c4f7368edcadcd0d
wslyvh <wslyvh@users.noreply.github.com>
2021-08-27 11:03:17 +0200

    dropdown

---
+++ src/hooks/useOnOutsideClick.tsx
@@ -0,0 +1,24 @@
+import { useEffect, RefObject } from 'react'
+
+type AnyEvent = MouseEvent | TouchEvent
+
+export const useOnOutsideClick = <T extends HTMLElement = HTMLElement>(ref: RefObject<T>, handler: (event: AnyEvent) => void) => {
+    useEffect(() => {
+        const listener = (event: AnyEvent) => {
+            const el = ref?.current
+            if (!el || el.contains(event.target as Node)) {
+                return
+            }
+
+            handler(event)
+        }
+
+        document.addEventListener(`mousedown`, listener)
+        document.addEventListener(`touchstart`, listener)
+
+        return () => {
+            document.removeEventListener(`mousedown`, listener)
+            document.removeEventListener(`touchstart`, listener)
+        }
+    }, [ref, handler])
+}
