1532c8614b783f3e2f55cf943b86afdfc18f57ec
wslyvh <wslyvh@users.noreply.github.com>
2021-10-17 10:19:05 +0200

    remove google

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { GA_TRACKING_ID } from 'utils/constants'
 
 export default class AppDocument extends Document {
   render() {
@@ -7,20 +6,6 @@ export default class AppDocument extends Document {
       <Html lang="en">
         <Head>
           <meta charSet="utf-8" />
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${GA_TRACKING_ID}');
-            `,
-            }}
-          />
         </Head>
         <body>
           <Main />
