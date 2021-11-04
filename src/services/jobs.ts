bfb95641f68c715616ce492b2db32164fd3b15b4
wslyvh <wslyvh@users.noreply.github.com>
2021-11-04 20:43:34 +0100

    sitemap

--- src/services/jobs.ts
+++ src/services/jobs.ts
@@ -25,38 +25,38 @@ export class JobService implements JobServiceInterface {
       const leverService = new LeverJobService()
       const workableService = new WorkableJobService()
 
-        try {
-          if (companyId) {
-            const breezy = JOBS_BREEZY.some(i => i === companyId)
-            if (breezy) jobs = await breezyService.GetJobs(companyId, maxItems)
-            
-            const greenhouse = JOBS_GREENHOUSE.some(i => i === companyId)
-            if (greenhouse) jobs = await greenhouseService.GetJobs(companyId, maxItems)
+      try {
+        if (companyId) {
+          const breezy = JOBS_BREEZY.some(i => i === companyId)
+          if (breezy) jobs = await breezyService.GetJobs(companyId, maxItems)
+          
+          const greenhouse = JOBS_GREENHOUSE.some(i => i === companyId)
+          if (greenhouse) jobs = await greenhouseService.GetJobs(companyId, maxItems)
 
-            const lever = JOBS_LEVER.some(i => i === companyId)
-            if (lever) jobs = await leverService.GetJobs(companyId, maxItems)
+          const lever = JOBS_LEVER.some(i => i === companyId)
+          if (lever) jobs = await leverService.GetJobs(companyId, maxItems)
 
-            const workable = JOBS_WORKABLE.some(i => i === companyId)
-            if (workable) jobs = await workableService.GetJobs(companyId, maxItems)
-          }
-          else {
-            const breezyJobs = JOBS_BREEZY.map(item => breezyService.GetJobs(item, maxItems))
-            const greenhouseJobs = JOBS_GREENHOUSE.map(item => greenhouseService.GetJobs(item, maxItems))
-            const leverJobs = JOBS_LEVER.map(item => leverService.GetJobs(item, maxItems))          
-            const workableJobs = JOBS_WORKABLE.map(item => workableService.GetJobs(item, maxItems))
-            
-            // Get all jobs
-            jobs = (await Promise.all([breezyJobs, greenhouseJobs, leverJobs, workableJobs].flat())).flat()
-          }
-
-          return jobs
-            .filter(i => companyId ? i.company.id === companyId : true)
-            .sort((a, b) => b.updated - a.updated)
-        } catch (e) {
-          console.log('GetJobs', 'Unable to fetch jobs', companyId)
-          console.error(e)
+          const workable = JOBS_WORKABLE.some(i => i === companyId)
+          if (workable) jobs = await workableService.GetJobs(companyId, maxItems)
         }
-    
-        return []
+        else {
+          const breezyJobs = JOBS_BREEZY.map(item => breezyService.GetJobs(item, maxItems))
+          const greenhouseJobs = JOBS_GREENHOUSE.map(item => greenhouseService.GetJobs(item, maxItems))
+          const leverJobs = JOBS_LEVER.map(item => leverService.GetJobs(item, maxItems))
+          const workableJobs = JOBS_WORKABLE.map(item => workableService.GetJobs(item, maxItems))
+          
+          // Get all jobs
+          jobs = (await Promise.all([breezyJobs, greenhouseJobs, leverJobs, workableJobs].flat())).flat()
+        }
+
+        return jobs
+          .filter(i => companyId ? i.company.id === companyId : true)
+          .sort((a, b) => b.updated - a.updated)
+      } catch (e) {
+        console.log('GetJobs', 'Unable to fetch jobs', companyId)
+        console.error(e)
+      }
+  
+      return []
     }
 }
