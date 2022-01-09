e9afb069d590908966f05be0c26187340d43e1b2
wslyvh <wslyvh@users.noreply.github.com>
2022-01-09 18:11:01 +0100

    start md migration

---
+++ src/migrate.ts
@@ -0,0 +1,71 @@
+import fs from 'fs'
+import moment from 'dayjs'
+import { AirtableItemService } from "./services/airtable"
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+// NOTE: tsconfig -> "module": "commonjs", instead of esnext
+
+console.log("Airtabe -> to markdown migration..")
+run()
+
+async function run() {
+    const service = new AirtableItemService()
+
+    // Categories
+    console.log('Getting all categories..')
+    const categories = await service.GetCategories()
+    console.log('Categories to migrate', categories.length)
+
+    const baseFolder = 'content'
+    categories.forEach(category => {
+        const dir = baseFolder + '/' + category.id 
+        if (!fs.existsSync(dir)) {
+            console.log('Create dir', dir)
+            fs.mkdirSync(dir)
+        }
+
+        if (!fs.existsSync(`${dir}/metadata.json`)) {
+            console.log('Create metadata file...')
+            fs.writeFileSync(`${dir}/metadata.json`, JSON.stringify(category, null, 4))            
+        }
+    })
+    console.log('Categories finished!')
+
+    // Items 
+    console.log('Getting all items..')
+    const items = await service.GetItems()
+    console.log('Items to migrate', items.length, items[0])
+
+    items.forEach(item => {
+        const file = `${baseFolder}/${item.category.id}/${item.id}.md`
+        if (!fs.existsSync(file)) {
+            const attributes = Object.entries(item)
+            let markdown = `---${attributes.reduce((acc, [key, value], index) => {
+                if (key === 'id' || key === 'category' || key === 'content' || key === 'created') return acc += ''
+                if (key === 'featured' && !value) return acc += ''
+
+                if (typeof value === 'number') {
+                    const date = new Date(value)
+                    return acc += `\n${key}: ${moment(date).format('YYYY-MM-DD')}`
+                }
+                if (typeof value === 'undefined') return acc += `\n${key}: ''`
+                if (typeof value === 'string') return acc += `\n${key}: '${value.trim()}'`
+                if (typeof value === 'object' && Array.isArray(value)) {
+                    return acc += `\n${key}: [${value.map(item => `'${item}'`)}]`
+                }
+
+                return acc += `\n${key}: '${value}'`
+            }, '')}\n---\n`
+
+            const content = attributes.find(i => i[0] === 'content')
+            if (content && content.length > 1 && content[1]) {
+                markdown += `\n${content[1]}`
+            }
+
+            console.log('Create markdown file...')
+            fs.writeFileSync(file, markdown)
+        }
+    })
+    console.log('Items finished!')
+}
---
+++ src/services/content.ts
@@ -0,0 +1,56 @@
+
+import fs from 'fs'
+import { join } from 'path'
+import matter from 'gray-matter'
+import { Category } from 'types/category'
+import { ContentItem } from 'types/content-item'
+
+const baseFolder = 'content'
+
+export function GetCategories(): Array<Category> {
+    const dir = join(process.cwd(), baseFolder)
+    const dirs = fs.readdirSync(dir, { withFileTypes: true }).filter(i => i.isDirectory())
+
+    return dirs.map(i => {
+        const fullPath = join(dir, i.name, 'metadata.json')
+        const content = fs.readFileSync(fullPath, 'utf8')
+
+        if (!content) {
+            console.log('File has no content..', i.name)
+        }
+        
+        return JSON.parse(content) as Category
+    })
+}
+
+export function GetCategory(id: string): Category | undefined {
+    return GetCategories().find(i => i.id === id)
+}
+
+export function GetItems(category: string, featured?: boolean): Array<ContentItem> {
+    const cat = GetCategory(category)
+    if (!cat) return[]
+    
+    const dir = join(process.cwd(), baseFolder, category)
+    const files = fs.readdirSync(dir, { withFileTypes: true }).filter(i => i.isFile() && i.name.endsWith('.md'))
+
+    const items = files.map(i => {
+        const fullPath = join(dir, i.name)
+        const content = fs.readFileSync(fullPath, 'utf8')
+
+        if (!content) {
+            console.log('File has no content..', i.name)
+        }
+
+        const doc = matter(content)
+        return { 
+            ...doc.data,
+            id: i.name.replace('.md', ''),
+            featured: doc.data.featured ?? false,
+            date: new Date(doc.data.date as string).getTime(),
+            content: doc.content
+        } as ContentItem
+    }).filter(i => !!i) 
+    
+    return items.filter(i => featured ? i.featured === featured : true)   
+}
