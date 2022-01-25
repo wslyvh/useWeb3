3819e6bc3ff17b86ef4731a4b2464209449a77d4
Sean <seanlengws@gmail.com>
2022-01-25 18:52:18 +0800

    [Added] Theme switcher with dark theme variables

--- src/components/layouts/main.tsx
+++ src/components/layouts/main.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useRef, useState } from "react";
+import { ReactNode, useRef, useState, useEffect } from "react";
 import styles from "./main.module.scss";
 import { Sitenav } from "components/sitenav";
 import { Link } from "components/link";
@@ -15,6 +15,7 @@ type Props = {
 
 export function Main(props: Props) {
   const [isMobileNavOpen, setMobileNav] = useState(false);
+  const [currentTheme, setCurrentTheme] = useState("light");
   const buttonRef = useRef<HTMLButtonElement>(null);
   const handleCLick = () => {
     setMobileNav((state) => !state);
@@ -22,12 +23,22 @@ export function Main(props: Props) {
   const title = props.title ?? "useWeb3";
 
   let className = `${styles.container}`;
-  if (props.className) className += ` ${props.className}`;
+  if (props.className) className += ` ${props.className} ${currentTheme}`;
 
   return (
     <div className={className}>
       <aside className={styles.sitenav}>
         <Sitenav />
+        <button className={styles.darkModeButton} onClick={() => setCurrentTheme(currentTheme === "light" ? "dark" : "light")}>
+          <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+            />
+          </svg>
+        </button>
       </aside>
       <aside className={styles.mobileSitenav}>
         <MobileNav isOpen={isMobileNavOpen} />
@@ -46,8 +57,7 @@ export function Main(props: Props) {
 
           <footer className={styles.footer}>
             <p>
-              Follow @ <Link href="https://twitter.com/useWeb3">useWeb3</Link>.
-              Contribute on{" "}
+              Follow @ <Link href="https://twitter.com/useWeb3">useWeb3</Link>. Contribute on{" "}
               <Link href="https://github.com/wslyvh/useWeb3">Github</Link>.
             </p>
             <p>
