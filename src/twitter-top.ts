14005e15eb09a50f2ef538e46522e4386048a9ef
wslyvh <wslyvh@users.noreply.github.com>
2022-01-25 11:35:59 +0100

    combined top tweet

--- src/twitter-top.ts
+++ src/twitter-top.ts
@@ -11,22 +11,37 @@ run()
 const EXCLUDE = ['https://github.com/wslyvh/useWeb3/tree/main/content']
 
 async function run() {
-    console.log('TOP')
-    const service = new MarkdownContentService()
-    
+    // Top resources / outside clicks
     const response = await fetch(`https://plausible.io/api/v1/stats/breakdown?site_id=useweb3.xyz&period=7d&property=event:props:url&filters=event:name==Outbound+Link%3A+Click`, {
         headers: { 'Authorization': `Bearer ${process.env.PLAUSIBLE_API_KEY}` },
     })
-    
     const body = await response.json()
-    const stats = body.results.map((i: any) => i.url).filter((i: any) => !EXCLUDE.includes(i)).splice(0, 5)
+    const stats = body.results.splice(0, 10)
+
+    // Most popular pages
+    const responsePopular = await fetch('https://plausible.io/api/v1/stats/breakdown?site_id=useweb3.xyz&period=7d&property=event:page&limit=10', {
+        headers: { 'Authorization': `Bearer ${process.env.PLAUSIBLE_API_KEY}` },
+    })
+    const bodyPopular = await responsePopular.json()
+    const pagesPopular = bodyPopular.results.filter((i: any) => i.page !== '/' && i.page.split('/').length > 2)
+        .map((i: any) => { return {
+            url: i.page,
+            visitors: i.visitors
+        }})
+
+    const combined = [...stats, pagesPopular].flat()
+        .sort((a: any, b: any) => b.visitors - a.visitors)
+        .map((i: any) => i.url).filter((i: any) => !EXCLUDE.includes(i))
+        .splice(0, 5)
+    
+    const service = new MarkdownContentService()
     const items = await service.GetItems()
 
     let text = `Most popular last week 🚀\n\n`
-    for (let index = 0; index < stats.length; index++) {
-        const resource = items.find(i => i.url === stats[index])
+    for (let index = 0; index < combined.length; index++) {
+        const resource = items.find(i => i.url === combined[index])
         if (resource) {
-            text += `${index + 1}. ${resource.title} ${resource.authors.join(' ')} \n`
+            text += `${index + 1}. ${resource.category.emoji} ${resource.title} ${resource.authors.join(' ')} \n`
         }
     }
 
