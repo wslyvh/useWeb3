e4a77e54ec5b9ac8bdfaff2872004d449207bbaf
Sean <seanlengws@gmail.com>
2022-01-26 16:25:50 +0800

    [Added] Color theme to sidenav

--- src/components/layouts/main.tsx
+++ src/components/layouts/main.tsx
@@ -35,13 +35,8 @@ export function Main(props: Props) {
       <aside className={styles.sitenav}>
         <Sitenav />
         <button className={styles.darkModeButton} onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
-          <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-            />
+          <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
           </svg>
         </button>
       </aside>
