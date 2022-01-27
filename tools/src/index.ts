6cefada913a3a9542e7a595b6d7de5b7eff2b297
wslyvh <wslyvh@users.noreply.github.com>
2022-01-27 11:52:51 +0100

    tyrael

---
+++ tools/src/index.ts
@@ -0,0 +1,16 @@
+import { join } from 'path'
+import fs from 'fs'
+
+const main = async () => {
+  const data = join(__dirname, '../../data')
+  console.log('Files in data directory', data)
+  fs.readdirSync(data, { withFileTypes: true }).forEach(i => {
+    console.log('  -', i.name)
+  })
+
+  const file = join(__dirname, '../../data', 'jobs.json')
+  console.log('Creating file', file)
+  fs.writeFileSync(file, new Date().getTime().toString())
+}
+
+main()
