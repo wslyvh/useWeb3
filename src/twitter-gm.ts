83c32d734ccced312d64595b6df3b14e43402ca4
wslyvh <wslyvh@users.noreply.github.com>
2022-02-16 10:00:28 +0100

    auto tweets

---
+++ src/twitter-gm.ts
@@ -0,0 +1,39 @@
+import * as dotenv from 'dotenv'
+const Twit = require('twit')
+
+dotenv.config()
+
+console.log('Tweet on first Monday of the month')
+run()
+
+async function run() {
+  const dayOfMonth = new Date().getDate()
+  if (dayOfMonth > 7) return
+
+  const twitterClient = new Twit({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  })
+
+  const text = `
+gm ☀️
+
+What are you all working on this week? 🤓`
+
+  try {
+    twitterClient.post('statuses/update', { status: text }, function (err: any, data: any, response: any) {
+      if (err) {
+        console.log('Unable to post Twitter update..')
+        console.error(err)
+      } else {
+        console.log('Update posted to Twitter..')
+        console.log(`https://twitter.com/useWeb3/status/${data.id_str}`)
+      }
+    })
+  } catch (e) {
+    console.log('Unable to post Twitter update..')
+    console.error(e)
+  }
+}
