a3163ce2e899309b386a907ecb2df77f0e9f6000
wslyvh <wslyvh@users.noreply.github.com>
2022-02-23 09:24:00 +0100

    social desc

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 export const TITLE = 'useWeb3.xyz'
 export const DESCRIPTION =
-  'A curated overview of the best and latest resources on Ethereum, blockchain and Web3 development.'
-export const SITE_URL = 'https://useweb3.xyz/'
+  'A platform for developers to learn about Web3. Explore tutorials, guides, courses, challenges and more. Learn by building ⚒️ Find your job ✨ Start contributing'
+export const SITE_URL = 'https://www.useweb3.xyz/'
 export const IMAGE_OG = `${SITE_URL}images/social.png`
 export const SOCIAL_HANDLE = '@useWeb3'
 export const DOMAIN = 'useweb3.xyz'
