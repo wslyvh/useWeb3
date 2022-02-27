61e5f761d69cf7d05dd41e446675cc733a426dc9
wslyvh <wslyvh@users.noreply.github.com>
2022-02-27 12:05:37 +0100

    gas fetcher

--- src/tools/gas-fetch.ts
+++ src/tools/gas-fetch.ts
@@ -10,9 +10,9 @@ run()
 type GasPrice = {
   id: number
   baseFee: number
-  prioritySlow: number,
-  priorityNormal: number,
-  priorityFast: number,
+  prioritySlow: number
+  priorityNormal: number
+  priorityFast: number
   created: string
 }
 
@@ -26,11 +26,7 @@ async function run() {
 
   const historicalBlocks = 24
   const provider = new JsonRpcProvider(`https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`)
-  const eth_feeHistory = await provider.send('eth_feeHistory', [
-    historicalBlocks,
-    'latest',
-    [1, 50, 90],
-  ])
+  const eth_feeHistory = await provider.send('eth_feeHistory', [historicalBlocks, 'latest', [1, 50, 90]])
 
   const latestBaseFees = eth_feeHistory.baseFeePerGas.map((i: any) => Number(i))
   const medianBaseFee = getMedian(latestBaseFees)
@@ -51,11 +47,11 @@ async function run() {
       baseFee,
       prioritySlow,
       priorityNormal,
-      priorityFast
+      priorityFast,
     },
   ])
 
-  if (insert.error) { 
+  if (insert.error) {
     console.log('Unable to save fee history..')
     throw new Error(insert.error.message)
   }
