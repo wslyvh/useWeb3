535a5df9c0af51ff0f339e5e818913415eff689a
wslyvh <wslyvh@users.noreply.github.com>
2022-02-28 13:17:17 +0100

    latest resources

---
+++ src/tools/latest.ts
@@ -0,0 +1,27 @@
+import * as dotenv from 'dotenv'
+import moment from 'moment'
+import { MarkdownContentService } from '../services/content'
+const Twit = require('twit')
+
+dotenv.config()
+
+console.log('Get Latest resources')
+run()
+
+async function run() {
+  const since = moment.utc().subtract(30, 'day')
+  const service = new MarkdownContentService()
+  const categories = await service.GetCategories()
+  const items = await service.GetItems()
+  const recent = items.filter((i) => moment(i.dateAdded) >= since)
+  console.log('Resources since', since.toISOString(), recent.length)
+  console.log('')
+
+  let text = `New resources ✨\n\n`
+  for (const item of recent) {
+    const category = categories.find((i) => i.id === item.category.id)
+    text += `${category?.emoji} ${item.title} ${item.authors.join(' ')} \n`
+  }
+  
+  console.log(text)
+}
