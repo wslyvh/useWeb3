6f47281a238ba09ba7040e1526c5ddb9f2238663
wslyvh <wslyvh@users.noreply.github.com>
2022-07-22 12:15:01 +0200

    tweet random tip

---
+++ src/services/tips.ts
@@ -0,0 +1,23 @@
+import * as dotenv from 'dotenv'
+import Airtable from "airtable"
+
+dotenv.config()
+
+if (!process.env.AIRTABLE_API_KEY || !process.env.AIRTABLE_API_KEY) {
+  throw new Error('Airtable API Base or Key not set.')
+}
+
+const client: Airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
+const base: Airtable.Base = client.base(process.env.AIRTABLE_API_BASE ?? '')
+
+export async function GetRandomTip(): Promise<string | undefined> {
+  try {
+    const records = await base('Tips').select().all()
+    const tips = records.map((source) => source.fields['Text'] as string)
+
+    return tips[Math.floor(Math.random() * tips.length)]
+  } catch (e) {
+    console.log('GetTips', 'Unable to fetch tips')
+    console.error(e)
+  }
+}
---
+++ src/tools/twitter-tip.ts
@@ -0,0 +1,30 @@
+import * as dotenv from 'dotenv'
+import { GetRandomTip } from '../services/tips'
+const Twit = require('twit')
+
+dotenv.config()
+
+console.log('Tweet random tip')
+run()
+
+async function run() {
+  const twitterClient = new Twit({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  })
+
+  const text = await GetRandomTip()
+  if (text) {
+    const response = await twitterClient.post('statuses/update', { status: text })
+    if (response.err) {
+      console.log('Unable to post Twitter update..')
+      console.error(response.err)
+    }
+    if (response.data) {
+      console.log(`https://twitter.com/useWeb3/status/${response.data.id_str}`)
+    }
+  }
+
+}
