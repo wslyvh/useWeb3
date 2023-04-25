6dd80ccdc0b77eab552d4093a30fd37c108cbf95
wslyvh <wslyvh@users.noreply.github.com>
2023-04-25 16:39:34 +0200

    add gitcoin grants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,7 +5,8 @@ export const SITE_URL = 'https://www.useweb3.xyz/'
 export const IMAGE_OG = `${SITE_URL}images/social.png`
 export const SOCIAL_HANDLE = '@useWeb3'
 export const DOMAIN = 'useweb3.xyz'
-export const GITCOIN_GRANT = 'https://gitcoin.co/grants/4143/wslyvh-useweb3-ethgas-tokenlog-more'
+export const GITCOIN_GRANT =
+  'https://explorer.gitcoin.co/#/round/1/0x12bb5bbbfe596dbc489d209299b8302c3300fa40/0x12bb5bbbfe596dbc489d209299b8302c3300fa40-56'
 export const NEWSLETTER_URL = 'https://useweb3.substack.com/'
 
 export const DEFAULT_REVALIDATE_PERIOD = 14400
